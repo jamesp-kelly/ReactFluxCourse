465e40671b15cb63bf18c5d4084c8f4bce04a77e
James Kelly <jk_jameskelly@outlook.com>
2017-03-14 15:32:33 -0400

    finish intro chapter

---
+++ src/components/common/header.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var React = require('react');
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand" style={{"padding-top": "0px"}}>
+            <img src="images/logo.png" style={{height: "50px", width: "50px"}} />
+          </a>
+          <ul className="nav navbar-nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/#about">About</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+});
+
+module.exports = Header;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ var jquery = $;
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
 (function(win) {
   'use strict';
@@ -22,6 +23,7 @@ var About = require('./components/about/aboutPage');
 
       return (
         <div>
+          <Header/>
           <Child/>
         </div>
       );
