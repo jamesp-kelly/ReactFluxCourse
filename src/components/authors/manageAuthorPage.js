8c8c3f60d7305019cdeecb735fe853f9104c9c61
James Kelly <jk_jameskelly@outlook.com>
2017-03-15 16:20:20 -0400

    validation and proptypes

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,6 +4,7 @@ var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 //controller view
 var ManageAuthorPage = React.createClass({
@@ -12,7 +13,8 @@ var ManageAuthorPage = React.createClass({
   ],
   getInitialState: function() {
     return {
-      author: {id: '', firstName: '', lastName: ''}
+      author: {id: '', firstName: '', lastName: ''},
+      errors: {}
     };
   },
   setAuthorState: function(event) { //called each keypress
@@ -21,9 +23,33 @@ var ManageAuthorPage = React.createClass({
     this.state.author[field] = value;
     return this.setState({author: this.state.author});
   },
+  authorFormIsValid: function() {
+    var formIsValid = true;
+    this.state.errors = {}; //clear any previous errors 
+
+    if (this.state.author.firstName.length < 3) {
+      this.state.errors.firstName = 'First name must be at least 3 characters';
+      formIsValid = false;
+    }
+
+    if (this.state.author.lastName.length < 3) {
+      this.state.errors.lastName = 'Last name must be at least 3 characters';
+      formIsValid = false;
+    }
+
+    this.setState({errors: this.state.errors});
+    
+    return formIsValid;
+  },
   saveAuthor: function(event) {
     event.preventDefault();
+
+    if (!this.authorFormIsValid()) {
+      return;
+    }
+
     AuthorApi.saveAuthor(this.state.author);
+    toastr.success('Author saved');
     this.transitionTo('authors');
   },
   render: function() {
@@ -31,7 +57,8 @@ var ManageAuthorPage = React.createClass({
         <AuthorForm 
           author={this.state.author}
           onChange={this.setAuthorState}
-          onSave={this.saveAuthor} />
+          onSave={this.saveAuthor}
+          errors={this.state.errors} />
     );
   }
 });
