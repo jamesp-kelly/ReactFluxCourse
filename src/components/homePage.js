ead22e30064be7225ecc160bf11998591d6e36f5
James Kelly <jk_jameskelly@outlook.com>
2017-03-15 11:43:37 -0400

    Links

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var React = require('react'); //commonjs pattens
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({ //es5 syntax
   render: function() {
@@ -8,6 +10,7 @@ var Home = React.createClass({ //es5 syntax
       <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
         <p>React, React Router and Flux for ultra-responsive web apps.</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
     );
   }
